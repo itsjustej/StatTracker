7d0c0fa6dc6dc1a08c191cc7376af716a35a6c0b
antoniodesravines <antonio.desravines@gmail.com>
2025-11-21 03:44:28 -0500

    old server

--- backend/server.js
+++ backend/server.js
@@ -4,17 +4,23 @@ import cors from "cors";
 import playerRoutes from "./routes/playerRoutes.js";
 import teamRoutes from "./routes/teamRoutes.js";
 import gameRoutes from "./routes/gameRoutes.js";
-import teamStatsRoutes from "./routes/teamStats.js";   
+import teamStatsRoutes from "./routes/teamStats.js";
+import leagueLeaderRoutes from "./routes/leagueLeaderRoutes.js";
 
 const app = express();
+
+// --- MIDDLEWARE ---
 app.use(cors());
 app.use(express.json());
 
+// --- ROUTES ---
 app.use("/api/players", playerRoutes);
 app.use("/api/teams", teamRoutes);
 app.use("/api/games", gameRoutes);
-app.use('/api/teamstats', teamStatsRoutes);          
+app.use("/api/teamstats", teamStatsRoutes);
+app.use("/api/league-leaders", leagueLeaderRoutes);
 
+// --- START SERVER ---
 app.listen(5000, () => {
   console.log("Server running on http://localhost:5000");
 });
